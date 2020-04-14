ce11431c27743bc9667a0a583b7d39866d8a6798
Claudia Pierson <c172@umbc.edu>
2020-04-14 05:39:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,7 @@
 class Dog 
-    def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
- 
-  def name
-    @this_dogs_name
+  
   def bark 
     puts "woof!"
-end 
 end 
 end 
 fido = Dog.new 
